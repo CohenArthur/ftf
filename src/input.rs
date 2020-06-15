2b068727a28925f18e5cf52c0648e83ef0f64a07
cohenarthur <arthur.cohen@epita.fr>
2020-06-15 04:40:02 +0200

    input: Switch from Duration to u64

--- src/input.rs
+++ src/input.rs
@@ -2,19 +2,22 @@
 //! to conditions that `ft` understands. Add any language by implementing
 //! the FtInput trait
 
-use std::time::Duration;
-
 use serde::Deserialize;
 
 #[derive(Deserialize)]
-pub struct Input {
+pub struct TestCase {
     pub name: String,
     pub cmd: String,
     pub args: Option<Vec<String>>,
     pub stdout: Option<String>,
     pub stderr: Option<String>,
     pub exit_code: Option<u8>,
-    pub timeout: Option<Duration>,
+    pub timeout: Option<u64>,
+}
+
+#[derive(Deserialize)]
+pub struct Input {
+    pub tests: Vec<TestCase>,
 }
 
 pub trait FtInput {
--- src/yaml/mod.rs
+++ src/yaml/mod.rs
@@ -23,14 +23,40 @@ mod tests {
     use super::*;
 
     #[test]
-    fn deserialize() {
+    fn base_input() {
         let data = r#"
-          name: "YAML test case"
-          cmd: "yaml_parser"
+        tests:
+          - name: "YAML test case"
+            cmd: "yaml_parser"
         "#;
 
         let input = Yaml::parse(data);
 
-        assert_eq!(input.name, "YAML test case");
+        assert_eq!(input.tests[0].name, "YAML test case");
+        assert_eq!(input.tests[0].cmd, "yaml_parser");
+    }
+
+    #[test]
+    fn all_fields() {
+        let data = r#"
+        tests:
+          - name: "YAML test case"
+            cmd: "yaml_parser"
+            args:
+                - "yml0"
+                - "yml1"
+                - "yml2"
+            stdout: "YAML Output"
+            stderr: "YAML Errput"
+            exit_code: 47
+            timeout: 1600
+        "#;
+
+        let input = Yaml::parse(data);
+
+        assert_eq!(input.tests[0].stdout, Some(String::from("YAML Output")));
+        assert_eq!(input.tests[0].stderr, Some(String::from("YAML Errput")));
+        assert_eq!(input.tests[0].exit_code, Some(47));
+        assert_eq!(input.tests[0].timeout, Some(1600));
     }
 }
