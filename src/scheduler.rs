cb58313ac1ecd1ea5d30f13b75d8dd426212c431
cohenarthur <arthur.cohen@epita.fr>
2020-06-15 05:10:21 +0200

    scheduler: Add base for dispatch

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,18 +1,36 @@
 //! The scheduler handles a set of launchers, deciding who to launch and who
 //! to wait for.
 
-use crate::launcher::Launcher;
+use crate::yaml::Yaml;
+
+use crate::input::{Input, FtInput};
 use crate::output::Output;
+use crate::launcher::Launcher;
+use crate::args::FtArgs;
+
+use std::path::PathBuf;
+use std::fs;
 
 pub struct Scheduler {
     launchers: Vec<Launcher>,
 }
 
 impl Scheduler {
-    pub fn new(launchers: Vec<Launcher>) -> Scheduler {
-        Scheduler {
-            launchers,
+    fn dispatch_file(file: &PathBuf) -> Input {
+        // FIXME: Cleanup
+        match file.extension().unwrap().to_str() {
+            Some("yml") => Yaml::parse(&fs::read_to_string(file).unwrap()),
+            None => panic!("Not a valid filename {}", file.to_str().unwrap())
         }
+    }
+
+    pub fn from_args(args: &FtArgs) -> Scheduler {
+        let mut input_vec = Vec::new();
+
+        for file in args.files {
+            input_vec.push(dispatch_file(file));
+        }
+
     }
 
     pub fn run() -> Vec<Output> {
